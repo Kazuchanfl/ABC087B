8fd1a6725b21c88193df376504632fd72da44d49
髙橋和晃 <kazuaki.flute@gmail.com>
2020-10-06 09:38:58 +0900

    add memo

--- main.go
+++ main.go
@@ -18,5 +18,9 @@ func main() {
 	fmt.Fscan(r, &b)
 	fmt.Fscan(r, &c)
 	fmt.Fscan(r, &x)
+	// 500円玉を0からAまで指定する
+	  // 100円玉を0からBまで指定する
+	    // 50円玉を0からCまで指定する
+	      // 500 * a + 100 * b + 50 * c == x なら result ++
 	fmt.Fprint(w, result)
 }
