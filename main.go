6503092a2c6a6d54b4c2829b1aeb4e9fd6faefb4
髙橋和晃 <kazuaki.flute@gmail.com>
2020-10-08 09:55:20 +0900

    add for loop

--- main.go
+++ main.go
@@ -19,8 +19,10 @@ func main() {
 	fmt.Fscan(r, &c)
 	fmt.Fscan(r, &x)
 	// 500円玉を0からAまで指定する
-	  // 100円玉を0からBまで指定する
-	    // 50円玉を0からCまで指定する
-	      // 500 * a + 100 * b + 50 * c == x なら result ++
+	for i := 0; i < a; i++ {
+		// 100円玉を0からBまで指定する
+		// 50円玉を0からCまで指定する
+		// 500 * a + 100 * b + 50 * c == x なら result ++
+	}
 	fmt.Fprint(w, result)
 }
