1e00cce6bc6d2099ba217899192f06a68c24a4d8
髙橋和晃 <kazuaki.flute@gmail.com>
2020-10-10 08:58:19 +0900

    add 50 yen loop

--- main.go
+++ main.go
@@ -21,9 +21,11 @@ func main() {
 	// 500円玉を0からAまで指定する
 	for i := 0; i < a; i++ {
 		// 100円玉を0からBまで指定する
-		for j :=0; j < b; j++ {
+		for j := 0; j < b; j++ {
 			// 50円玉を0からCまで指定する
-			// 500 * a + 100 * b + 50 * c == x なら result ++
+			for k := 0; k < c; k++ {
+				// 500 * a + 100 * b + 50 * c == x なら result ++
+			}
 		}
 	}
 	fmt.Fprint(w, result)
